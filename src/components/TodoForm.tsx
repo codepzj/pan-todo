0188248d5e52b63d5632e3f1777171302e21c12e
pzj <2363435714@qq.com>
2025-12-14 19:05:52 +0800

    refactor(TodoForm): Remove unused lucide-react imports

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Check, X } from 'lucide-react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
